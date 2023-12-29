3e841e0c2b624257a1a3e54fd4983229ef43cfb4
Kirtiraj17 <kirtirajg1997@gmail.com>
2023-12-29 11:06:18 +0530

    about layout fix

--- src/app/(components)/About.jsx
+++ src/app/(components)/About.jsx
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <div id="about" className="w-full md:h-screen p-2 py-16 flex items-center">
       <div className="w-full mx-auto max-w-7xl">
-        <h2 className="text-blue-600 mb-4">About</h2>
-        <span className="text-2xl font-bold mb-4">Who I Am</span>
         <div className="m-auto max-w-7xl md:flex md:justify-between my-4">
           <div className="mb-4 md:basis-2/3 md:mr-8">
-            <p className="mb-4">
+            <h2 className="text-blue-600 mb-4">About</h2>
+            <span className="text-2xl font-bold mb-4">Who I Am</span>
+            <p className="my-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
               eligendi at sunt blanditiis. Hic laboriosam maiores qui
               dignissimos eius, totam enim tempore cumque voluptate dicta
